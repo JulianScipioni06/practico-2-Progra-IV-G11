5df8d4a6d37b2f5b4c98f3a7cb2b24ef3cdcf4f1
JulianScipioni06 <scipionijulian@gmail.com>
2026-09-25 20:19:54 -0300

    feat: 2 (verde)

--- tests/unit/noteService.list.test.ts
+++ tests/unit/noteService.list.test.ts
@@ -19,4 +19,20 @@ describe('NoteService - listNote (Ejercicio 2)', () => {
         // Assert: Forzamos el error a propósito para el commit rojo
         expect(notas.length).toBe(0); 
     });
+
+    it('debería devolver una lista con los elementos guardados', () => {
+        //Creamos una nota de prueba
+        service.createNote({
+            title: 'Aprobar Progra',
+            content: 'Terminar el ejercicio 2'
+        });
+
+        //Llamamos a la función para listar
+        const notas = service.listNotes();
+
+        //Comprobamos que la lista tenga 1 elemento y coincida con lo que creamos
+        expect(notas.length).toBe(1);
+        expect(notas[0].title).toBe('Aprobar Progra');
+        expect(notas[0].content).toBe('Terminar el ejercicio 2');
+    });
 });
